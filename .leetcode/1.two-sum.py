488a5c393dbb2ac3787d5c3a431c4b1ba95dcf49
ishumann <mann.agrawal17@gmail.com>
2025-01-07 19:13:50 +0530

    Two Sum solution

--- .leetcode/1.two-sum.py
+++ .leetcode/1.two-sum.py
@@ -1,17 +1,16 @@
 #
-# @lc app=leetcode id=1 lang=python
+# @lc app=leetcode id=1 lang=python3
 #
 # [1] Two Sum
 #
 
-# @lc code=start
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        
-# @lc code=end
 
+# @lc code=start
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[j] == target - nums[i]:
+                    return [i, j]
+        # Return an empty list if no solution is found
+        return []
